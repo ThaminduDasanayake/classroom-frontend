68f1de7f3c3c8016de2239542628918853d41048
Thamindu Dasanayake <thamindudasanayake@gmail.com>
2026-02-06 16:58:40 +0530

    Add toast for rate limiting

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,10 +1,26 @@
 import { createDataProvider, CreateDataProviderOptions } from '@refinedev/rest';
 import { BACKEND_BASE_URL } from '@/constants';
 import { ListResponse } from '@/types';
+import { HttpError } from '@refinedev/core';
 
 if (!BACKEND_BASE_URL) {
   throw new Error('BACKEND_BASE_URL environment variable must be a valid URL');
 }
+
+const buildHttpError = async (response: Response): Promise<HttpError> => {
+  let message = 'Request Failed';
+
+  try {
+    const payload = (await response.json()) as { message?: string };
+
+    if (payload?.message) message = payload.message;
+  } catch {}
+
+  return {
+    message,
+    statusCode: response.status,
+  };
+};
 
 const options: CreateDataProviderOptions = {
   getList: {
@@ -31,12 +47,16 @@ const options: CreateDataProviderOptions = {
     },
 
     mapResponse: async (response) => {
+      if (!response.ok) throw await buildHttpError(response);
+
       const payload: ListResponse = await response.clone().json();
 
       return payload.data ?? [];
     },
 
     getTotalCount: async (response) => {
+      if (!response.ok) throw await buildHttpError(response);
+
       const payload: ListResponse = await response.clone().json();
 
       return payload.pagination?.total ?? payload.data?.length ?? 0;
